725328a5695fe524d69f5e11c9d5b220441da4b7
Christie Lynam <christielynam@gmail.com>
2018-11-02 13:01:10 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const Toaster = styled.div`
+  width: 500px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px 25px 5px 5px;
+  background-color: silver
+`
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      toastColor: '',
+      counter: 15,
+      randomNum: null
+    }
+  }
+
+  countDown = () => {
+    this.setState({ counter: this.state.counter - 1 })
+    if(this.state.counter < 1) { 
+      clearInterval(this.timer);
+    }
+  }
+
+  setToastColor = (num) => {
+    if(num > 10) {
+      this.setState({ toastColor: 'light' })
+    }
+    if(num < 11 && num > 5) {
+      this.setState({ toastColor: 'medium' })
+    }
+    if(num < 6) {
+      this.setState({ toastColor: 'dark'})
+    }
+  }
+
+  toastDone = () => {
+    const num = Math.floor(Math.random() * 16)
+    this.setState({ randomNum: num })
+    this.setToastColor(num)
+    if(num === this.state.counter) {
+
+    }
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.countDown, 1000);
+    this.toastDone()
+  }
+
   render() {
+    const { counter, randomNum } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Toaster>
+        <div 
+          className={`${this.state.toastColor} toast`}
+          style={{visibility: randomNum >= counter ? 'visible' : 'hidden'}}
+        >
+        </div>
+        <h2>{counter}</h2>
+      </Toaster>
     );
   }
 }
